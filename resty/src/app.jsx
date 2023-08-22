e65e6a32501d0b65a400b176b2525dadd698e22d
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-08-22 08:34:36 +0000

    reducer Hook

--- resty/src/app.jsx
+++ resty/src/app.jsx
@@ -1,42 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 import Form from './components/form/index';
-import Results from './components/results/index'; 
+import Results from './components/results/index';
+import History from './components/History/index';
 import Header from './components/header/index';
 import Footer from './components/footer/index';
 import './app.scss';
+// import { apiCall } from '../utils/api';
+
+
+const initialState = {
+  loading: false,
+  results: null,
+  history: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
+    case 'SET_RESULTS':
+      return { ...state, results: action.payload };
+    case 'ADD_TO_HISTORY':
+      return { ...state, history: [...state.history, action.payload] };
+    default:
+      return state;
+  }
+};
+
 
 function App() {
   const [request, setRequest] = useState(null);
-  const [response, setResponse] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     if (request) {
+      dispatch({ type: 'SET_LOADING', payload: true });
       axios({
         method: request.method,
         url: request.url,
         data: request.body,
       })
         .then((res) => {
-          setResponse(res.data);
+          dispatch({ type: 'SET_RESULTS', payload: res.data });
+          dispatch({ type: 'ADD_TO_HISTORY', payload: { method: request.method, url: request.url, results: res.data } });
         })
         .catch((error) => {
-          setResponse({ error: error.message });
+          dispatch({ type: 'SET_RESULTS', payload: { error: error.message } });
+        })
+        .finally(() => {
+          dispatch({ type: 'SET_LOADING', payload: false });
         });
     }
   }, [request]);
 
   const handleFormSubmit = (requestData) => {
     setRequest(requestData);
-    setResponse(null); 
   };
 
   return (
     <div>
-      <Header/>
+      <Header />
       <Form onSubmit={handleFormSubmit} />
-      {response && <Results data={response} />}
-      <Footer/>
+      <History history={state.history} onSelectHistory={(results) => dispatch({ type: 'SET_RESULTS', payload: results })} />
+      <Results results={state.results} loading={state.loading} />
+      <Footer />
     </div>
   );
 }
